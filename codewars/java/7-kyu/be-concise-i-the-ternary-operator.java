cd017b12bbd38f01bbeb2083588c546ec4b902ab
RATHAN K <167230786+RATHAN005@users.noreply.github.com>
2026-02-07 00:12:25 +0530

    Be Concise I - The Ternary Operator

---
+++ codewars/java/7-kyu/be-concise-i-the-ternary-operator.java
@@ -0,0 +1,30 @@
+public class C {
+  public static String describeAge(int a) {
+    return "You're a(n) " + (a <= 12 ? "kid" : a <= 17 ? "teenager" : a <= 64 ? "adult" : "elderly");
+  }
+}
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void basicTests() {
+      assertEquals("You're a(n) kid", C.describeAge(9));
+      assertEquals("You're a(n) kid", C.describeAge(10));
+      assertEquals("You're a(n) kid", C.describeAge(11));
+      assertEquals("You're a(n) kid", C.describeAge(12));
+      assertEquals("You're a(n) teenager", C.describeAge(13));
+      assertEquals("You're a(n) teenager", C.describeAge(14));
+      assertEquals("You're a(n) teenager", C.describeAge(15));
+      assertEquals("You're a(n) teenager", C.describeAge(16));
+      assertEquals("You're a(n) teenager", C.describeAge(17));
+      assertEquals("You're a(n) adult", C.describeAge(18));
+      assertEquals("You're a(n) adult", C.describeAge(19));
+      assertEquals("You're a(n) adult", C.describeAge(63));
+      assertEquals("You're a(n) adult", C.describeAge(64));
+      assertEquals("You're a(n) elderly", C.describeAge(65));
+      assertEquals("You're a(n) elderly", C.describeAge(66));
+      assertEquals("You're a(n) elderly", C.describeAge(100));
+    }
